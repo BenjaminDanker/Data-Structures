16841f3b7225b0579eb6072750b965020230cb88
silvergem95@gmail.com <silvergem95@gmail.com>
2024-02-08 11:29:24 -0600

    Homework 2 - Add Comments

--- DataStructures/DataStructures.cpp
+++ DataStructures/DataStructures.cpp
@@ -2,21 +2,25 @@
 #include <vector>
 #include <numeric>
 
-// TODO: implement this function to return a Pascal Triangle
+
 std::vector<std::vector<int>> pascalTriangle(int row) {
 
     std::vector<std::vector<int>> triangle;
 
+    // row iteration
     for (int i = 0; i <= row; i++) {
         triangle.push_back({});
         int a = 1;
+        // column iteration
         for (int j = 0; j < i+1; j++) {
+            // start of triangle
             if (j == 0) {
                 triangle[i].push_back(1);
             }
             else {
                 triangle[i].push_back(a);
             }
+            // number in triangle is equal to the sum of the two numbers above it
             a = a * (i - j) / (j + 1);
         }
     }
@@ -24,7 +28,6 @@ std::vector<std::vector<int>> pascalTriangle(int row) {
     return triangle;
 }
 
-// TODO: implement this function to print Pascal Triangles 
 void printPascalTriangle(const std::vector<std::vector<int>>& triangle) {
     for (int i = 0; i <= 8; i++) {
         for (int j = 0; j <= i; j++) {
@@ -34,12 +37,12 @@ void printPascalTriangle(const std::vector<std::vector<int>>& triangle) {
     }
 }
 
-// TODO: implement this function to return comparison result. See main() for usage. 
 std::vector<bool> compare(const std::vector<std::vector<int>>& triangle,
     const std::vector<int> allegedSummations) {
     std::vector<bool> equalVec;
 
     int rowSum = 0;
+    // row iteration
     for (int i = 0; i <= allegedSummations.size()-1; i++) {
         rowSum = std::accumulate(triangle[i].begin(), triangle[i].end(), 0);
         
