b5c6d61a025d582f4c069074e1db684781527f75
BenjaminDanker <silvergem95@gmail.com>
2024-02-23 11:05:04 -0600

    Lab 4 - Template Double Linked List Constructor(), Deconstructor(), addBack(), addFront(), printForward(), printBackward()

--- DataStructures/DataStructures.cpp
+++ DataStructures/DataStructures.cpp
@@ -4,29 +4,34 @@
 #include <stdexcept>
 
 
+template <typename T>
 struct Node {
-    int data;
-    Node* next;
+    T data;
+    Node<T>* next;
+    Node<T>* prev;
 };
 
-class LinkedList {
+template <typename T>
+class DoubleLinkedList {
 private:
-    int count;
-    Node* begin;
+    Node<T>* begin;
+    Node<T>* end;
 
-    Node* makeNode(const int value) {
-        Node* newNode = new Node;
+    template <typename T>
+    Node<T>* makeNode(T value) {
+        Node<T>* newNode = new Node<T>;
 
         newNode->data = value;
         newNode->next = nullptr;
+        newNode->prev = nullptr;
 
         return newNode;
     }
 public:
-    LinkedList() : count{ 0 }, begin{ nullptr } {}
+    DoubleLinkedList() : begin{ nullptr }, end{ nullptr } {}
 
-    ~LinkedList() {
-        Node* del = begin;
+    ~DoubleLinkedList() {
+        Node<T>* del = begin;
 
         while (begin != nullptr) {
             begin = begin->next;
@@ -35,166 +40,85 @@ public:
         }
     }
 
-    void insert(int pos, int value) {
-        if (pos < 0 || pos > count) {
-            throw std::out_of_range("Position is out of range");
-        }
+    template <typename T>
+    void addBack(T value) {
+        Node<T>* newNode = makeNode(value);
 
-        Node* add = makeNode(value);
-        if (pos == 0) {
-            add->next = begin;
-            begin = add;
+        if (begin == nullptr) {
+            begin = newNode;
+            end = newNode;
         }
         else {
-            Node* cur = begin;
-
-            for (int i = 1; i < pos; i++) {
-                cur = cur->next;
-            }
-            add->next = cur->next;
-            cur->next = add;
-        }
-
-        count++;
-    }
-
-    LinkedList mergeSortedLists(LinkedList& list2) {
-        LinkedList list3;
-        Node* cur1 = begin;
-        Node* cur2 = list2.begin;
-
-        int i = 0;
-        while (cur1 != nullptr && cur2 != nullptr) {
-            if (cur1->data < cur2->data) {
-                list3.insert(i, cur1->data);
-                cur1 = cur1->next;
-
-                i++;
-            }
-            else if(cur1->data > cur2->data) {
-                list3.insert(i, cur2->data);
-                cur2 = cur2->next;
-
-                i++;
-            }
-            else if (cur1->data == cur2->data) {
-                list3.insert(i, cur1->data);
-                cur1 = cur1->next;
-                i++;
-
-                list3.insert(i, cur2->data);
-                cur2 = cur2->next;
-                i++;
-            }
-        };
-
-        return list3;
-    }
-
-    void rearrangeLinkedList() {
-        Node* prev = nullptr;
-        Node* cur = begin;
-
-        // set end to end of list
-        Node* end = begin;
-        while (end->next != nullptr) {
+            newNode->prev = end;
+            end->next = newNode;
             end = end->next;
         }
+    }
 
-        Node* newEnd = end;
-        while (cur->data % 2 == 0 && cur != end) {
-            newEnd->next = cur;
-            cur = cur->next;
+    template <typename T>
+    void addFront(T value) {
+        Node<T>* newNode = makeNode(value);
 
-            newEnd->next->next = nullptr;
-            newEnd = newEnd->next;
-        }
-
-        if (cur->data % 2 != 0) {
-            begin = cur;
-
-            while (cur != end) {
-                if (cur->data % 2 != 0) {
-                    prev = cur;
-                    cur = cur->next;
-                }
-                else {
-                    prev->next = cur->next;
-                    cur->next = nullptr;
-
-                    newEnd->next = cur;
-                    newEnd = cur;
-                    
-                    cur = prev->next;
-                }
-            }
+        if (begin == nullptr) {
+            begin = newNode;
+            end = newNode;
         }
         else {
-            prev = cur;
-        }
-
-        if (newEnd != end && end->data % 2 == 0) {
-            prev->next = end->next;
-            end->next = nullptr;
-            newEnd->next = end;
+            newNode->next = begin;
+            begin->prev = newNode;
+            begin = newNode;
         }
     }
 
-    void print() const {
-        if (count == 0) {
-            std::cout << "List is empty" << std::endl;
-        }
-        Node* cur = begin;
+    void printForward() const {
+        Node<T>* cur = begin;
         while (cur != nullptr) {
             std::cout << cur->data << std::endl;
             cur = cur->next;
         }
 
     }
+
+    void printBackward() const {
+        Node<T>* cur = end;
+        while (cur != nullptr) {
+            std::cout << cur->data << std::endl;
+            cur = cur->prev;
+        }
+    }
 };
 
 int main() {
-    // Create the first linked list: 1->3->5->7
-    LinkedList list1;
+    DoubleLinkedList<int> list1;
 
-    list1.insert(0, 1);
-    list1.insert(1, 3);
-    list1.insert(2, 5);
-    list1.insert(3, 7);
+    list1.addFront(1);
+    list1.addFront(2);
+    list1.addFront(3);
+    list1.addBack(4);
 
-    // Create the second linked list: 2->4->6->8
-    LinkedList list2;
-
-    list2.insert(0, 2);
-    list2.insert(1, 4);
-    list2.insert(2, 6);
-    list2.insert(3, 8);
-
-    // Merge the two sorted lists
-    LinkedList sortedList = list1.mergeSortedLists(list2);
-
-    // Print the merged list
-    std::cout << "Sorted List: " << std::endl;
-    sortedList.print();
+    list1.printForward();
+    std::cout << std::endl;
+    list1.printBackward();
 
 
+    std::cout << std::endl;
 
-    // Create the linked list: 1 -> 2 -> 5 -> 8 -> 3 -> 6 -> 7 -> 4
-    LinkedList list3;
 
-    list3.insert(0, 1);
-    list3.insert(1, 2);
-    list3.insert(2, 5);
-    list3.insert(3, 8);
-    list3.insert(4, 3);
-    list3.insert(5, 6);
-    list3.insert(6, 7);
-    list3.insert(7, 4);
+    DoubleLinkedList<std::string> list2;
 
-    // Rearrange the list
-    list3.rearrangeLinkedList();
+    // compiler will assume (const char*) if passed in variables are not declared as string first no matter the length
+    std::string one = "1";
+    std::string two = "2";
+    std::string three = "3";
+    std::string four = "4";
 
-    // Print the rearranged list
-    std::cout << "Rearranged List: " << std::endl;
-    list3.print();
+
+    list2.addFront(one);
+    list2.addFront(two);
+    list2.addFront(three);
+    list2.addBack(four);
+
+    list2.printForward();
+    std::cout << std::endl;
+    list2.printBackward();
 }
